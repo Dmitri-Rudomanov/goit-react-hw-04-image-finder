bce0bfdb2759eb2a4720be0259b646fd440a9927
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-03-20 10:06:14 +0200

    search-form-refactor

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,48 +1,42 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import s from './SearchForm.module.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class SearchForm extends Component {
-  state = {
-    query: '',
+export default function SearchForm({ onSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const handleQueryChange = event => {
+    setQuery(event.currentTarget.value);
   };
 
-  handleQueryChange = event => {
-    this.setState({ query: event.currentTarget.value });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       toast.error('Please enter your search query.');
       return;
     }
 
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
+    onSubmit(query);
+    setQuery('');
   };
 
-  render() {
-    return (
-      <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-        <button type="submit" className={s.SearchFormButton}>
-          <span className={s.SearchFormButtonLabel}>Search</span>
-        </button>
+  return (
+    <form className={s.SearchForm} onSubmit={handleSubmit}>
+      <button type="submit" className={s.SearchFormButton}>
+        <span className={s.SearchFormButtonLabel}>Search</span>
+      </button>
 
-        <input
-          className={s.SearchFormInput}
-          type="text"
-          autoComplete="off"
-          value={this.state.query}
-          autoFocus
-          placeholder="Search images and photos"
-          onChange={this.handleQueryChange}
-        />
-      </form>
-    );
-  }
+      <input
+        className={s.SearchFormInput}
+        type="text"
+        autoComplete="off"
+        value={query}
+        autoFocus
+        placeholder="Search images and photos"
+        onChange={handleQueryChange}
+      />
+    </form>
+  );
 }
-
-export default SearchForm;
